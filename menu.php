ab8380a182edd11779a8e9db1c5da175d683a352
Janberbayev <37609982+Janberbayev@users.noreply.github.com>
2018-03-22 01:59:33 +0600

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,4 @@
+<?php
+$menu=array(array('t1'=>'index.php?page=t1','label'=>'newTask'), array('url'=>'index.php?page=t2',
+		'label'=>'completedTask'), array('url'=>'index.php?page=t3', 'label'=>'canceledTask'));
+?>
